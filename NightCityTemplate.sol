88ce3575eab019440610da53498f0019f595dc46
morenabarboni <morena.barboni@unicam.it>
2025-03-10 13:19:23 +0100

    Update template

--- NightCityTemplate.sol
+++ NightCityTemplate.sol
@@ -1,38 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// **Import ERC721**
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NightCity is ERC721URIStorage, Ownable {
-    
-    // **This struct defines the properties of a Land**
+
+    // **Land Structure**
     struct Land {
-       
+
+    }
+    // **Proposal Structure**
+    struct Proposal {
+
     }
 
-    // **Global Variables will go here ...**
+    // **Global Variables**
 
+    // **Mappings**
 
-    // **Event Definitions will go here ...**
+    // **Events**
+    event LandMinted(uint256 indexed tokenId, address indexed minter, string district);
+    event LandListedForSale(uint256 indexed tokenId, uint256 price);
+    event LandRented( uint256 indexed tokenId,address indexed renter, uint256 expiration);
+    event ReputationIncreased(address indexed user, uint256 newReputation);
+    event ProposalCreated(uint256 indexed proposalId, string description, uint256 expirationTime);
+    event ProposalVoted(uint256 indexed proposalId, address indexed voter, bool inFavor);
+    event ProposalExecuted(uint256 indexed proposalId, bool accepted);
 
-
-    // **The contract constructor defines the token name and symbol of the Land NFT**
     constructor() ERC721("Night City Land", "LAND") Ownable(msg.sender) {}
 
-    // **The mintLand function creates a new LAND NFT**
-    function mintLand(string memory _tokenURI, uint256 _price) public {
-       
+    // **Mint Land with District Assignment**
+    function mintLand(string memory _tokenURI, uint256 _price, string memory _district) public {
+        
+
+        emit LandMinted(tokenId, msg.sender, _district);
     }
 
-    // **The listForSale function lists a LAND NFT for sale**
+    // **List Land for Sale**
     function listForSale(uint256 tokenId, uint256 price) public {
        
+        emit LandListedForSale(tokenId, price);
     }
 
-    // **The rentLand function lists a LAND NFT for rent**
+    // **Rent Land**
     function rentLand(uint256 tokenId, uint256 duration) public payable {
        
+        emit LandRented(tokenId, msg.sender, lands[tokenId].rentExpiration);
     }
+
+    // **Governance: Create Proposal**
+    function createProposal(string memory description, uint256 duration) public {
+
+        emit ProposalCreated(nextProposalId, description, proposals[nextProposalId].expirationTime);
+    }
+
+    // **Governance: Vote on Proposal**
+    function voteProposal(uint256 proposalId, bool inFavor) public {
+
+        emit ProposalVoted(proposalId, msg.sender, inFavor);
+    }
+
+
+    // **Execute Proposal**
+    function executeProposal(uint256 proposalId) public onlyOwner {
+      emit ProposalExecuted(proposalId, proposals[proposalId].accepted);
+    }
+
+    // **Increase Reputation**
+    function increaseReputation(address user, uint256 points) internal {
+        
+        emit ReputationIncreased(user, reputationPoints[user]);
+    }
+
 }
